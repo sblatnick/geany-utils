d9832afaab927c3400ebb82d021dbc894d3d6046
Steven Blatnick <steve8track@yahoo.com>
2021-06-26 20:13:28 -0600

    getting things ready for the plugin

--- quick-opener.c
+++ quick-opener.c
@@ -1,33 +1,41 @@
 #include <geanyplugin.h>
 
-static void item_activate_cb(GtkMenuItem *menuitem, gpointer user_data)
+static void quick_open(GtkMenuItem *menuitem, gpointer user_data)
 {
-    dialogs_show_msgbox(GTK_MESSAGE_INFO, "Hello World");
+    dialogs_show_msgbox(GTK_MESSAGE_INFO, _("Hello World"));
 }
 static gboolean init(GeanyPlugin *plugin, gpointer pdata)
 {
-    GtkWidget *main_menu_item;
-    // Create a new menu item and show it
-    main_menu_item = gtk_menu_item_new_with_mnemonic("Hello World");
-    gtk_widget_show(main_menu_item);
-    gtk_container_add(GTK_CONTAINER(plugin->geany_data->main_widgets->tools_menu),
-            main_menu_item);
-    g_signal_connect(main_menu_item, "activate",
-            G_CALLBACK(item_activate_cb), NULL);
-    geany_plugin_set_data(plugin, main_menu_item, NULL);
+    GtkWidget *menu_entry;
+    menu_entry = gtk_menu_item_new_with_mnemonic(_("Quick Open..."));
+    gtk_widget_show(menu_entry);
+    gtk_container_add(
+      GTK_CONTAINER(
+        plugin->geany_data->main_widgets->tools_menu
+      ),
+      menu_entry
+    );
+    g_signal_connect(
+      menu_entry,
+      "activate",
+      G_CALLBACK(quick_open),
+      NULL
+    );
+    geany_plugin_set_data(plugin, menu_entry, NULL);
     return TRUE;
 }
 static void cleanup(GeanyPlugin *plugin, gpointer pdata)
 {
-    GtkWidget *main_menu_item = (GtkWidget *) pdata;
-    gtk_widget_destroy(main_menu_item);
+    GtkWidget *menu_entry = (GtkWidget *) pdata;
+    gtk_widget_destroy(menu_entry);
 }
 
 G_MODULE_EXPORT
 void geany_load_module(GeanyPlugin *plugin)
 {
-  plugin->info->name = "Quick Opener";
-  plugin->info->description = "Search filenames while typing";
+  main_locale_init("share/locale", "quick-opener");
+  plugin->info->name = _("Quick Opener");
+  plugin->info->description = _("Search filenames while typing");
   plugin->info->version = "0.2";
   plugin->info->author = "Steven Blatnick <steve8track@yahoo.com>";
   plugin->funcs->init = init;
